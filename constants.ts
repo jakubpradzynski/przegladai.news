d23cbd09c976899fe525d7baaa23b7d2d73c7369
Jakub Prądzyński <jakubpradzynski@gmail.com>
2026-01-09 07:34:21 +0100

    Issue #2

--- constants.ts
+++ constants.ts
@@ -7,5 +7,12 @@ export const BLOG_POSTS: BlogPost[] = [
     title: 'Wydanie #1: hiszpański Jesus AI i miliardowe zakupy Zuckerberga w Chinach',
     date: '04-01-2025',
     htmlUrl: '/issues/1.html'
+  },
+  {
+    id: '2',
+    slug: 'wydanie-2-nowosci-z-targow-ces-i-tryb-health-w-chatgpt',
+    title: 'Wydanie #2: nowości z targów CES i tryb Health w ChatGPT',
+    date: '09-01-2025',
+    htmlUrl: '/issues/2.html'
   }
 ];
