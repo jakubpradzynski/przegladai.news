f3fad0ccf61dd2c605ec70ac8543dca5343dd50f
Jakub Prądzyński <jakubpradzynski@gmail.com>
2026-01-12 20:43:27 +0100

    Fix issue dates and order

--- constants.ts
+++ constants.ts
@@ -2,17 +2,17 @@ import { BlogPost } from './types';
 
 export const BLOG_POSTS: BlogPost[] = [
   {
-    id: '1',
-    slug: 'wydanie-1-hiszpanski-jesus-ai-i-miliardowe-zakupy-zuckerberga-w-chinach',
-    title: 'Wydanie #1: hiszpański Jesus AI i miliardowe zakupy Zuckerberga w Chinach',
-    date: '04-01-2025',
-    htmlUrl: '/issues/1.html'
-  },
-  {
     id: '2',
     slug: 'wydanie-2-nowosci-z-targow-ces-i-tryb-health-w-chatgpt',
     title: 'Wydanie #2: nowości z targów CES i tryb Health w ChatGPT',
-    date: '09-01-2025',
+    date: '09-01-2026',
     htmlUrl: '/issues/2.html'
+  },
+  {
+    id: '1',
+    slug: 'wydanie-1-hiszpanski-jesus-ai-i-miliardowe-zakupy-zuckerberga-w-chinach',
+    title: 'Wydanie #1: hiszpański Jesus AI i miliardowe zakupy Zuckerberga w Chinach',
+    date: '04-01-2026',
+    htmlUrl: '/issues/1.html'
   }
 ];
